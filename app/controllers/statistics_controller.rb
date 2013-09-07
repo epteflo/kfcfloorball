ecd64bec30d8edf198a743e2d294db0dc95ebf1a
Bali Miklós <mike@dbx.hu>
2013-09-07 22:09:38 +0200

    statistics duplication bug workaround

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,7 @@ class StatisticsController < ActionController::Base
 
   def show
     #statistics = Checkin.group('user_id').sum(:goal, :assist)
-    statistics = Checkin.joins(:user).select("user_id, count(1) as matches, sum(goal) as goals, sum(assist) as assists").group("user_id")
+    statistics = Checkin.joins(:user).select("user_id as id, user_id, count(1) as matches, sum(goal) as goals, sum(assist) as assists").group("user_id")
     users = User.find(statistics.map(&:user_id))
     render json: {statistics: statistics, users: users}
   end
