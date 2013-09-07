c36613e579d739420541a6acdce4ee8f2ee5c7ef
Fuli <fuli@dbx.hu>
2013-09-07 15:41:08 +0200

    Matches kesz

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,7 @@
 class MatchesController < ActionController::Base
+
   def index
-    matches = Match.order("id desc")
+    matches = Match.where('state!="deleted"').order("id desc")
     render json: {matches: matches}
   end
 
@@ -13,4 +14,69 @@ class MatchesController < ActionController::Base
     end
     render json: {match: match, checkins: match.checkins, users: User.find(user_ids)}
   end
+
+  def create
+    match_to_save = Match.new(match_params)
+    match_to_save.save
+    render json: {match: match_to_save}
+  end
+
+  def update
+    match = Match.find(params[:id])
+    changed = false
+    unless(match.limit == params[:match][:limit])
+      match.limit = params[:match][:limit]
+      changed = true
+    end
+
+    unless(match.match_date == params[:match][:match_date])
+      match.match_date = params[:match][:match_date]
+      changed = true
+    end
+
+    unless(match.state == params[:match][:state])
+      match.state = params[:match][:state]
+      changed = true
+    end
+
+    unless(match.deadline == params[:match][:deadline])
+      match.deadline = params[:match][:deadline]
+      changed = true
+    end
+
+    unless(match.team_a_score == params[:match][:team_a_score])
+      match.team_a_score = params[:match][:team_a_score]
+      changed = true
+    end
+
+    unless(match.team_b_score == params[:match][:team_b_score])
+      match.team_b_score = params[:match][:team_b_score]
+      changed = true
+    end
+
+    unless(match.venue == params[:match][:venue])
+      match.venue = params[:match][:venue]
+      changed = true
+    end
+
+    if(changed)
+      match.save
+    end
+
+    render json: {match: match}
+
+  end
+
+  def destroy
+    match = Match.find(params[:id])
+    match.state = 'deleted'
+    match.save
+    render json: {match: match}
+  end
+
+  private
+
+    def match_params
+      params.require(:match).permit(:match_date, :limit, :state, :venue, :deadline)
+    end
 end
