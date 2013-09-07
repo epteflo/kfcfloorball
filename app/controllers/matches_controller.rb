c2d5fa368b9f7f13039e72114857fce57178892f
Fuli <fuli@dbx.hu>
2013-09-07 14:45:29 +0200

    Match show added

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,5 +4,13 @@ class MatchesController < ActionController::Base
     render json: {matches: matches}
   end
 
-  
+  def show
+    match = Match.find(params[:id])
+    checkins = match.checkins
+    user_ids = Array.new
+    checkins.each do |checkin|
+      user_ids.push(checkin.user_id)
+    end
+    render json: {match: match, checkins: match.checkins, users: User.find(user_ids)}
+  end
 end
