aca4b2ac6f0c14fce81344bfbd0fe03fd73bf0da
Fuli <fuli@dbx.hu>
2013-09-09 21:11:09 +0200

    Statistics ordered

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,7 @@ class StatisticsController < ApplicationController
 
   def show
     #statistics = Checkin.group('user_id').sum(:goal, :assist)
-    statistics = Checkin.joins(:user).select("user_id as id, user_id, count(1) as matches, sum(goal) as goals, sum(assist) as assists").group("user_id")
+    statistics = Checkin.joins(:user).select("user_id as id, user_id, count(1) as matches, sum(goal) as goals, sum(assist) as assists").group("user_id").order("goals desc")
     users = User.find(statistics.map(&:user_id))
     render json: {statistics: statistics, users: users}
   end
