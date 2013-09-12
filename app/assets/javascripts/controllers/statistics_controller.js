414364f4186bf75737ca840c418cd03968efd524
Bali Miklós <mike@dbx.hu>
2013-09-12 20:57:46 +0200

    dynamic stats ordering

--- app/assets/javascripts/controllers/statistics_controller.js
+++ app/assets/javascripts/controllers/statistics_controller.js
@@ -1,4 +1,16 @@
 App.StatisticsController = Em.ArrayController.extend({
   sortProperties: ['pointsPerMatch'],
-  sortAscending: false
+  sortAscending: false,
+  actions: {
+    toggleSort: function(sortBy) {
+      var previousSortBy;
+      previousSortBy = this.get('sortProperties.0');
+      if (sortBy === previousSortBy) {
+        return this.set('sortAscending', !this.get('sortAscending'));
+      } else {
+        this.set('sortAscending', true);
+        return this.set('sortProperties', [sortBy]);
+      }
+    }
+  }
 });
